93fc6fe0b03f6b4aab33a03e3d8d32b4c222aae9
19274 <1927435985@qq.com>
2025-04-16 19:08:16 +0800

    Initial commit

---
+++ src/User.java
@@ -0,0 +1,30 @@
+public class User {
+    private long id;
+    private String name;
+    private String gender;
+
+    // Getters and Setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
